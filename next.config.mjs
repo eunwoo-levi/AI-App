b7e8f379b6f34940191f6bd4483b2aa8b6189636
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-20 10:37:16 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
+
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
